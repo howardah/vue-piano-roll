4d7f87fded251dc0e6d8f0ccccf837012e61b225
Adam Howard <adam.thomas.howard@gmail.com>
2025-08-26 13:59:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite";
+import path from "node:path";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "vue-piano-roll",
-      fileName: (format) => `vue-piano-roll.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
-  },
-  plugins: [vue()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			name: "vue-piano-roll",
+			fileName: (format) => `vue-piano-roll.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["vue"],
+			output: {
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+	plugins: [vue()],
 });
