80a5b5987908fe127028b0b6669492ca4d41f077
Adam Howard <adam.thomas.howard@gmail.com>
2025-08-26 13:57:11 +0200

    Update version

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import PianoRoll from "./components/PianoRoll.vue";
 import {
-  PianoRollNote,
-  PianoRollProps,
-  PianoRollSimpleNote,
-  NoteEvent,
-  ShadowMap,
+	type Note,
+	notesBetweenC1AndG9,
+	type OctaveNote,
+} from "./assets/notes";
+import type {
+	NoteEvent,
+	PianoRollNote,
+	PianoRollProps,
+	PianoRollSimpleNote,
+	ShadowMap,
 } from "./assets/piano";
-import { OctaveNote, Note, notesBetweenC1AndG9 } from "./assets/notes";
+import PianoRoll from "./components/PianoRoll.vue";
 
 export { PianoRoll, notesBetweenC1AndG9 };
 export type {
-  PianoRollNote,
-  OctaveNote,
-  PianoRollProps,
-  PianoRollSimpleNote,
-  NoteEvent,
-  ShadowMap,
-  Note
+	PianoRollNote,
+	OctaveNote,
+	PianoRollProps,
+	PianoRollSimpleNote,
+	NoteEvent,
+	ShadowMap,
+	Note,
 };
